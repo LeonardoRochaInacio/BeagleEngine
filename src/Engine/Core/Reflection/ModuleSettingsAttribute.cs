e70a86b02ed569f6b6d5bfa0821093141ba82258
Leonardo <leo.rocha.inacio>
2019-03-27 08:23:22 -0300

    .

--- src/Engine/Core/Reflection/ModuleSettingsAttribute.cs
+++ src/Engine/Core/Reflection/ModuleSettingsAttribute.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 
 namespace Beagle.Core
 {
-    public static class leozin
-    {
-        public static int x;
-    }
-
     [AttributeUsage(AttributeTargets.Property)]
     public class ModuleSettingsAttribute : Attribute
     {
